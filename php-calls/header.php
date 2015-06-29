c8694f0c46e6d67b5ce983dfc67b577d43f53d85
Emily MaRee <lee@mleemaree.com>
2015-06-29 20:11:25 +0200

    FUCKING FONTS

--- php-calls/header.php
+++ php-calls/header.php
@@ -3,7 +3,7 @@
 <link rel="stylesheet" href="bower_components/bootstrap/dist/css/boostrap-theme.min.css">
 
 <!--JASNY-->
-<link rel="stylesheet" href="bower_components/jasny-bootstrap/dist/css/jasny-bootstrap.css">
+<link href='http://fonts.googleapis.com/css?family=Raleway:400,100,200,300,600,500,700,800,900' rel='stylesheet' type='text/css'>
 
 <!-- CSS -->
 <link href="/stylesheets/screen.css" media="screen, projection" rel="stylesheet" type="text/css" />
