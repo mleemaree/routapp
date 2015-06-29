36383fd3d18c07f1ec4a608e013f57b3f302763d
Emily MaRee <lee@mleemaree.com>
2015-06-29 22:28:47 +0200

    login

--- register-php-calls/login-orig.php
+++ register-php-calls/login-orig.php
@@ -1,37 +1,40 @@
-<?php 
++<?php 
 +session_start();
 +
 +$_SESSION['name']='user';
 +
 +if (isset($_POST['email']) and isset($_POST['password'])) {
 +
-+ require_once('conexion-routapp.php');
-+ 
-+ $sql="SELECT * FROM users WHERE email='".$_POST['email']."';";
-+ 
-+ if ($result = $conexion->query($sql)) {
-+   $fila = $result->fetch_assoc();
-+   if ($fila['password']!="" and $fila['password']==$_POST['password']) {
-+     $_SESSION['user']=$fila;
-+   } else {
-+     $_SESSION['user']=false;    
-+   }
-+   $result->close();
-+ }
-+ 
++	require_once('conexion-php-calls/conexion-routapp.php');
++	
++	$sql="SELECT * FROM users WHERE email='".$_POST['email']."';";
++	
++	if ($result = $conexion->query($sql)) {
++		$fila = $result->fetch_assoc();
++		if ($fila['password']!="" and $fila['password']==$_POST['password']) {
++			$_SESSION['user']=$fila;
++		} else {
++			$_SESSION['user']=false;		
++		}
++		$result->close();
++	}
++	
 +} 
 +
 +?>
-
-<?php if ($_SESSION['user'] and $_SESSION['user']['userID']!=0)  { 
 +
-+ ?> 
++<?php if ($_SESSION['user'] and $_SESSION['user']['userID']!=0)  { 
++
++	?> 
 +
 +
-+ <?php include('routes.php');
++	<?php include('routes.php');
 +
 +
 +} else { ?>
+
+
+
 
 		<form action="register-php-calls/login.php" method="post">
 
@@ -48,7 +51,7 @@
         <input type="submit" class="hvr-glow" value="Login">
       </form>
 
-+<?php } ?>
+<?php } ?>
 
 
 
