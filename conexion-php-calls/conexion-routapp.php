9ec941f36558cc5d57d42828393b59a22a61dfaa
Emily MaRee <lee@mleemaree.com>
2015-07-05 16:36:01 +0200

    conexion to data online

--- conexion-php-calls/conexion-routapp.php
+++ conexion-php-calls/conexion-routapp.php
@@ -1,9 +1,9 @@
 <?php
 
-$database = "routapp"; 
-$hostname = "localhost"; 
-$username = "root";
-$password = "root"; 
+$database = "db457893_6"; 
+$hostname = "mysql5.colortwister.abcde.biz"; 
+$username = "db457893_6";
+$password = "Routapp2015!"; 
 $conexion = new mysqli($hostname, $username, $password, $database);
 $conexion->set_charset("utf8");
 
