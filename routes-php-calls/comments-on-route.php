9a3ad4130590307f7ae71f802055415114d272f4
Emily MaRee <lee@mleemaree.com>
2015-07-06 18:51:36 +0200

    comments

--- routes-php-calls/comments-on-route.php
+++ routes-php-calls/comments-on-route.php
@@ -10,7 +10,7 @@ $_SESSION['name']='route';
 
  <div class="container" id="profile-comments">
 	<div class="row comment-title">
-		<div class="col-xs-10">
+		<div class="col-xs-10 comment-title-wrapper">
 			<h3>Comentarios (<?php echo $_SESSION['route']['comments'];?>)</h3>
 		</div>
 		<div class="col-xs-2">
@@ -57,24 +57,7 @@ $sql="SELECT * FROM comments WHERE (routeID = '".$_GET['routeID']."') ORDER BY `
 		</div>
 	</div>
 	<div class="hidden-xs col-sm-1"></div>
-	<div class="row comment-options">
-		<div class="col-xs-12 col-sm-6 pull-right">
-			<div class="row">
-				<div class="col-xs-7">
-					<p>Denunciar contenido inapropiado</p>
-					<img alt="denunciar" src="images/iconos/denunciar.png">
-				</div>
-				<div class="col-xs-3">
-					<p>Ver respuestas</p>
-					<img alt="ver" src="images/iconos/ver.png">
-				</div>
-				<div class="col-xs-2">
-					<p>Responder</p>
-					<img alt="responder" src="images/iconos/responder.png">
-				</div>
-			</div>
-		</div>
-	</div>
+	
 <hr>
 			    			
 							<?php    			
@@ -85,32 +68,43 @@ $sql="SELECT * FROM comments WHERE (routeID = '".$_GET['routeID']."') ORDER BY `
 
 			?>
 
-				¿Que te ha parecido la ruta? ¿Te ha gustado? Tu opinion es importante para nosotros y la comunidad de Routapp!
 
-			</p>
+	<div class="row make-comment">
+		<div class="row make-comment-title">
+			<div class="col-xs-12">
+				<h4 style="text-align:center; font-weight:bold;">¿Que te ha parecido la ruta? ¿Te ha gustado? Tu opinion es importante para nosotros y la comunidad de Routapp!</h4>
+			</div>
+		</div>
+		<form action="register-php-calls/comment-register.php?routeID=<?php echo $_GET['routeID']?>&username=<?php echo $_GET['username']?>" method="post" enctype="multipart/form-data">
+		<div class="row new-comment-title">
+			<div class="col-xs-12 col-sm-4 tit-row">
+				<label for="title">Titulo</label>
+			</div>
+			<div class="col-xs-12 col-sm-4 tit-row">
+				<input class="new-comment-titled" type="text" name="title">
+			</div>
+			<div class="col-xs-12 col-sm-4 tit-row">
+				<h4>Valora la ruta <img class="comment-rating"src="images/stars.png"></h4>
+			</div>
+		</div>
+		<div class="row comment-input">
+			<div class="col-xs-12 col-sm-8 col-sm-offset-2">
+				<textarea class="new-comment-text" class="comment-text" name="content"></textarea>
+			</div>
+		</div>
+		<div class="row comment-submit">
+			<div class="col-xs-12 col-sm-8 col-sm-offset-2 button-wrapper">
+				<input class="new-comment-submit" type="submit" value="Guardar">
+			</div>
+		</div>
+	</form>
+	</div>
+
+			
 
 
 
-			<form action="register-php-calls/comment-register.php?routeID=<?php echo $_GET['routeID']?>&username=<?php echo $_GET['username']?>" method="post" enctype="multipart/form-data">
 
-			<ul>
-
-				<li>
-					<p>Valora la ruta <img src="images/stars.png"></p>
-				</li>
-				<li>
-					<label for="title">Titulo</label>
-					<input type="text" name="title">
-				</li>
-				<li>
-					<textarea name="content"></textarea>
-				</li>
-			</ul>
-
-
-			<input type="submit" value="Guardar">
-
-			</form>
 
 </div>
 
