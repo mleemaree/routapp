4554f64786d1062777dd1936f9f634d9f61dc5f8
myronkaradimas <myron.kar@gmail.com>
2015-07-07 02:07:49 +0200

    single route pro map

--- routes-php-calls/single-route-user.php
+++ routes-php-calls/single-route-user.php
@@ -75,8 +75,34 @@ $sql2="SELECT * FROM zones where routeID=".$_GET['routeID'];
 	</div>
 </div>
 
+<!-- GOOGLE MAP DIV BEGIN -->
+
+    <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&signed_in=true"></script>
+    <script>
+function initialize() {
+  var barcelona = new google.maps.LatLng(41.3833,2.1833);
+  var mapOptions = {
+    zoom: 11,
+    center: barcelona
+  }
+
+  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+  var ctaLayer = new google.maps.KmlLayer({
+    url: 'http://a06.elisava.net/ikiMap_Routapp-Barcelona_Pe.kml?uwufwgffuwg'
+  });
+  ctaLayer.setMap(map);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
+    </script>
+
+
+<!-- GOOGLE MAP DIV END -->
+
 <div class="col-xs-12 user-map">
-	<img style="width:100%;" src="images/maps/map4.png">
+	<div id="map-canvas"></div>
 </div>
 
 
